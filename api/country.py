9318aef7f65c9dab1c796d1e56b36d4aa873549a
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-05-09 15:41:38 -0700

    removed commented out code

--- api/country.py
+++ api/country.py
@@ -4,31 +4,6 @@ import requests
 
 
 class handler(BaseHTTPRequestHandler):
-    # def do_GET(self):
-    #     # url_components = parse.urlsplit(self.path)
-    #     # query_string_list = parse.parse_qsl(url_components.query)
-    #     # dic = dict(query_string_list)
-    #     #
-    #     # if "name" in dic:
-    #     #     url = "https://restcountries.com/v3.1/capital"
-    #     #     query = dic['name']
-    #     #     full_url = url + query
-    #     #     response = requests.get(full_url)
-    #     #     data = response.json()
-    #     #     country = data[0]['name']
-    #     #     message = str(country['common'])
-    #     #     final_message = f"{query} is the Capital of {country}"
-    #     #     self.send_response(200)
-    #     #     self.send_header('Content-type', 'text/plain')
-    #     #     self.end_headers()
-    #     #     self.wfile.write(final_message.encode())
-    #     else:
-    #         message = "Please type a Capital in as your query to the url and then we'll find the Country!"
-    #         self.send_response(200)
-    #         self.send_header('Content-type', 'text/plain')
-    #         self.end_headers()
-    #         self.wfile.write(message.encode())
-    #     return
     def do_GET(self):
         url_components = parse.urlsplit(self.path)
         query_string_list = parse.parse_qsl(url_components.query)
