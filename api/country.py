28cf6e5dd717c6c68e8f00262af7e1db7f5a2fd3
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-05-09 15:41:21 -0700

    fixed up

--- api/country.py
+++ api/country.py
@@ -31,30 +31,23 @@ class handler(BaseHTTPRequestHandler):
     #     return
     def do_GET(self):
         url_components = parse.urlsplit(self.path)
-        query_list = parse.parse_qsl(url_components.query)  # returns list, ['name', 'peru']
-        dictionary = dict(query_list)  # turns list into dict
-        # print(dictionary)
-        if 'name' in dictionary:
-            query = dictionary['name']
-            # print(query)
+        query_string_list = parse.parse_qsl(url_components.query)
+        dic = dict(query_string_list)
+        if 'name' in dic:
+            query = dic['name']
             url = "https://restcountries.com/v3.1/capital/"
-            complete_url = url + dictionary['name']
-            # print(complete_url + ' this is your URL')
-            response = requests.get(complete_url)
+            full_url = url + dic['name']
+            response = requests.get(full_url)
             data = response.json()
-            country = data[0]['name']  # index's to the capital city.
-            print(country)
-
+            country = data[0]['name']
             message = str(country['common'])
             final = f'{query} is the capital of {message}'
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(final.encode())
-
         else:
-            message = "Type a country in the query to get its capital city."
-
+            message = "Please type a Capital in as your query to the url and then we'll find the Country!"
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
